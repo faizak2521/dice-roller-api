b775e6bc0af95ee6bc9dc810ac02f351297c5b88
Faiza Khan <161170273+faizak2521@users.noreply.github.com>
2025-10-02 20:42:22 -0500

    Added JSON, npm express, npm cors, nodemon, and added random dice logic to server.js and ran on local device to test for now.

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const port = 5500
+
+app.listen(port, () => console.log(`Server started on port: ${port}`))
+
+// Automatically sets HTTP hearders 
+app.use(cors())
+
+app.get('/api/random', (req, res) => {
+    // get min's actual value
+    const min = parseInt(req.query.min) || 1
+    const max = parseInt(req.query.max) || 6
+    const value = Math.floor(Math.random() * (max - min + 1)) + min
+    res.json({value})
+})
+
+
